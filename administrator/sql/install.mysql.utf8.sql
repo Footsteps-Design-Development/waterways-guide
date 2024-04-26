d700d227cf47c4ed8cbda5e5ede30ff15ed09a8a
Russell English <russell.english@stormit.co.uk>
2024-04-26 16:50:45 +0100

    Update install.mysql.utf8.sql

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -1,11 +1,5 @@
 CREATE TABLE IF NOT EXISTS `#__waterways_guide` (
   `GuideID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `state` TINYINT(1)  NULL  DEFAULT 1,
-  `ordering` INT(11)  NULL  DEFAULT 0,
-  `checked_out` INT(11)  UNSIGNED,
-  `checked_out_time` DATETIME NULL  DEFAULT NULL ,
-  `created_by` INT(11)  NULL  DEFAULT 0,
-  `modified_by` INT(11)  NULL  DEFAULT 0,
   `GuideNo` bigint(20) NOT NULL DEFAULT '0',
   `GuideVer` smallint(6) NOT NULL DEFAULT '0',
   `GuideCountry` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -33,29 +27,13 @@ CREATE TABLE IF NOT EXISTS `#__waterways_guide` (
   `GuideStatus` tinyint(3) UNSIGNED DEFAULT NULL,
   `GuideEditorMemNo` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`GuideID`)
-  ,KEY `idx_state` (`state`)
-  ,KEY `idx_checked_out` (`checked_out`)
-  ,KEY `idx_created_by` (`created_by`)
-  ,KEY `idx_modified_by` (`modified_by`)
-) DEFAULT COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `#__waterways_guide_requests` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `state` TINYINT(1)  NULL  DEFAULT 1,
-  `ordering` INT(11)  NULL  DEFAULT 0,
-  `checked_out` INT(11)  UNSIGNED,
-  `checked_out_time` DATETIME NULL  DEFAULT NULL ,
-  `created_by` INT(11)  NULL  DEFAULT 0,
-  `modified_by` INT(11)  NULL  DEFAULT 0,
   `memberid` bigint(20) NOT NULL DEFAULT '0',
   `GuideCountry` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `GuideWaterway` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `GuideRequestDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `GuideRequestMethod` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `GuideRequestStatus` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-  ,KEY `idx_state` (`state`)
-  ,KEY `idx_checked_out` (`checked_out`)
-  ,KEY `idx_created_by` (`created_by`)
-  ,KEY `idx_modified_by` (`modified_by`)
-) DEFAULT COLLATE=utf8mb4_unicode_ci;
+  `GuideRequestStatus` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
