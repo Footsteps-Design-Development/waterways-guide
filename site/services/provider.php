c9c9c24652654c777972bb1752a713d2b4d57e0c
Russell English <russell.english@stormit.co.uk>
2025-02-10 15:21:14 +0000

    Update provider.php

--- site/services/provider.php
+++ site/services/provider.php
@@ -6,16 +6,12 @@ use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\ComponentFactory;
-use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\CMS\MVC\Service\Provider\MVCFactory;
 use Joomla\CMS\Router\Service\Provider\RouterFactory;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Component\WaterWaysGuide\Site\Extension\WaterWaysGuideComponent;
 
-/**
- * The provider class for com_waterways_guide
- */
 return new class implements ServiceProviderInterface
 {
     public function register(Container $container)
@@ -24,6 +20,9 @@ return new class implements ServiceProviderInterface
         $container->registerServiceProvider(new MVCFactory());
         $container->registerServiceProvider(new ComponentDispatcherFactory());
         $container->registerServiceProvider(new RouterFactory());
+
+        // Load Helper
+        require_once JPATH_SITE . '/components/com_waterways_guide/src/Helper/WaterwaysHelper.php';
 
         $container->set(
             ComponentInterface::class . '.com_waterways_guide',
