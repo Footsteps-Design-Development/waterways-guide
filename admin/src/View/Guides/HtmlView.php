825cc03b8525b2345c88f5d9c91941cf472527a6
Russell English <russell.english@stormit.co.uk>
2026-02-23 16:54:17 +0000

    Fixing backend bugs

--- admin/src/View/Guides/HtmlView.php
+++ admin/src/View/Guides/HtmlView.php
@@ -6,59 +6,19 @@ namespace Joomla\Component\WaterWaysGuide\Administrator\View\Guides;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Factory;
 use Joomla\Component\WaterWaysGuide\Administrator\Helper\WaterWaysGuideHelper;
 
-/**
- * Guides List View
- */
 class HtmlView extends BaseHtmlView
 {
-    /**
-     * An array of items
-     *
-     * @var  array
-     */
     protected $items;
-
-    /**
-     * The pagination object
-     *
-     * @var  \Joomla\CMS\Pagination\Pagination
-     */
     protected $pagination;
-
-    /**
-     * The model state
-     *
-     * @var  \Joomla\CMS\Object\CMSObject
-     */
     protected $state;
-
-    /**
-     * Form object for filters
-     *
-     * @var  \Joomla\CMS\Form\Form
-     */
     public $filterForm;
-
-    /**
-     * Active filters
-     *
-     * @var  array
-     */
     public $activeFilters;
 
-    /**
-     * Display the view
-     *
-     * @param   string  $tpl  The template name
-     *
-     * @return  void
-     */
     public function display($tpl = null): void
     {
         $this->items         = $this->get('Items');
@@ -67,42 +27,33 @@ class HtmlView extends BaseHtmlView
         $this->filterForm    = $this->get('FilterForm');
         $this->activeFilters = $this->get('ActiveFilters');
 
-        // Check for errors
+        // Check for errors.
         if (count($errors = $this->get('Errors'))) {
-            throw new \Exception(implode("\n", $errors), 500);
+            throw new \RuntimeException(implode("\n", $errors), 500);
         }
-
-        // Add submenu
-        WaterWaysGuideHelper::addSubmenu('guides');
 
         $this->addToolbar();
 
         parent::display($tpl);
     }
 
-    /**
-     * Add the page title and toolbar
-     *
-     * @return  void
-     */
     protected function addToolbar(): void
     {
-        $canDo = WaterWaysGuideHelper::getActions();
-
         ToolbarHelper::title(Text::_('COM_WATERWAYS_GUIDE_GUIDES_TITLE'), 'location');
+
+        $canDo = WaterWaysGuideHelper::getActions();
 
         if ($canDo->get('core.create')) {
             ToolbarHelper::addNew('guide.add');
         }
 
-        if ($canDo->get('core.edit') || $canDo->get('core.edit.own')) {
+        if ($canDo->get('core.edit')) {
             ToolbarHelper::editList('guide.edit');
         }
 
         if ($canDo->get('core.edit.state')) {
             ToolbarHelper::publish('guides.publish', 'JTOOLBAR_PUBLISH', true);
             ToolbarHelper::unpublish('guides.unpublish', 'JTOOLBAR_UNPUBLISH', true);
-            ToolbarHelper::archiveList('guides.archive');
         }
 
         if ($canDo->get('core.delete')) {
@@ -114,20 +65,12 @@ class HtmlView extends BaseHtmlView
         }
     }
 
-    /**
-     * Returns an array of fields the table can be sorted by
-     *
-     * @return  array  Array containing the field name to sort by as the key and display text as value
-     */
     protected function getSortFields(): array
     {
         return [
             'a.GuideID'      => Text::_('COM_WATERWAYS_GUIDE_GUIDE_ID'),
             'a.GuideName'    => Text::_('COM_WATERWAYS_GUIDE_GUIDE_NAME'),
             'a.GuideCountry' => Text::_('COM_WATERWAYS_GUIDE_GUIDE_COUNTRY'),
-            'a.GuideWaterway'=> Text::_('COM_WATERWAYS_GUIDE_GUIDE_WATERWAY'),
-            'a.GuideStatus'  => Text::_('COM_WATERWAYS_GUIDE_GUIDE_STATUS'),
-            'a.GuideUpdate'  => Text::_('COM_WATERWAYS_GUIDE_GUIDE_UPDATE'),
         ];
     }
 }
