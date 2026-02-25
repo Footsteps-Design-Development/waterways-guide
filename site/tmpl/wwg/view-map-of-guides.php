60ab52b9f382dfa99092641703f9f6bf079d03da
Russell English <russell.english@stormit.co.uk>
2026-02-25 17:10:03 +0000

    Fixed missing ">"

--- site/tmpl/wwg/view-map-of-guides.php
+++ site/tmpl/wwg/view-map-of-guides.php
@@ -8,7 +8,7 @@ if (isset($message)) {
 echo ("<tr><td colspan=4><input type=\"button\" class=\"btn btn-primary button_action\" name=\"mapback\" value=\"Back to the filter\" onClick=\"document.form.guideaction.value='waterways';document.form.submit()\"><input type=\"button\" class=\"btn btn-primary\" name=\"listback\" value=\"Back to the list\" onClick=\"document.form.guideaction.value='list';document.form.lastguideaction.value='list';document.form.submit()\"></td></tr>\n");
 echo ("<tr><td colspan=4>");
 echo ("<input name=\"thisid\" type=\"hidden\" value=\"" . $thisid . "\">\n");
-echo '<div id="guides_map"';
+echo '<div id="guides_map">';
 include("guides_map.php");
 echo '</div>';
 echo ("</td></tr>\n");
